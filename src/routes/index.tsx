4bb1af4db90a25984c4ef0abae27a8ed45e9f8c6
hamkke <walwalzz@naver.com>
2022-06-03 19:58:22 +0900

    🎉  개인과제 시작

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,7 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Health from 'components/health'
-import Home from 'components/home'
-import Graph from 'components/graph'
 import Layout from 'components/layout'
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path='' element={<Home />} />
-          <Route element={<Health />} />
-          <Route element={<Graph />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+  return <div>안녕하세요</div>
 }
 
 export default App
