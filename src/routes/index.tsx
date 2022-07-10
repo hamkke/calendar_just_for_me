59660839a89cbdc834263b2dcd865c6ee0e62adc
hamkke <walwalzz@naver.com>
2022-07-10 23:03:22 +0900

    불필요한 코드 삭제

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,19 +4,8 @@ import { AppDispatch } from 'states'
 import { getStatusListFB, getRecapListFB } from 'states/userData'
 import Layout from 'components/layout'
 
-import { db } from '../firebase'
-import { setDoc, doc } from 'firebase/firestore'
-import DATA from 'data.json'
-
 const App = () => {
   const dispatch = useDispatch<AppDispatch>()
-
-  // useEffect(() => {
-  //   setDoc(doc(db, 'my-list', 'STATUS'), {
-  //     // recapList: [...DATA.recapList],
-  //     statusList: [...DATA.statusList],
-  //   })
-  // })
 
   useEffect(() => {
     dispatch(getStatusListFB())
